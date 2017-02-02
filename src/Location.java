52dab7a9afb0c8299b606ffa6615effb5763ff22
David Weng <dkweng@gmail.com>
2017-02-01 20:13:44 -0500

    Add Location Class

---
+++ src/Location.java
@@ -0,0 +1,12 @@
+/**
+ * Created by David on 2/1/2017.
+ */
+public class Location {
+    public int xIndex;
+    public int yIndex;
+
+    public Location(int xIndex, int yIndex){
+        this.xIndex=xIndex;
+        this.yIndex=yIndex;
+    }
+}
