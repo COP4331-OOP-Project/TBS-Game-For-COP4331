8bfb5dcf507b573daae852ca171de414e76cb17a
karthshen <karth-shen@hotmail.com>
2017-02-02 20:05:29 -0500

    Change variable names to lowercase

--- src/view/GamePanel.java
+++ src/view/GamePanel.java
@@ -36,17 +36,17 @@ public class GamePanel extends Panel {
 		for (int i = 0; i < 5; i++) {
 			for (int j = 0; j < 5; j++) {
 
-				if(gBoard.GameMap[i][j].getTileType()==0) {
-					g.drawImage(Assets.getInstance().getImage("TERRAIN_GRASS"), tileLocation(gBoard.GameMap[i][j].getlocation().xIndex),
-							tileLocation(gBoard.GameMap[i][j].getlocation().yIndex), null);
+				if(gBoard.gameMap[i][j].getTileType()==0) {
+					g.drawImage(Assets.getInstance().getImage("TERRAIN_GRASS"), tileLocation(gBoard.gameMap[i][j].getlocation().xIndex),
+							tileLocation(gBoard.gameMap[i][j].getlocation().yIndex), null);
 				}
-				if(gBoard.GameMap[i][j].getTileType()==1) {
-					g.drawImage(Assets.getInstance().getImage("TERRAIN_SAND"), tileLocation(gBoard.GameMap[i][j].getlocation().xIndex),
-							tileLocation(gBoard.GameMap[i][j].getlocation().yIndex), null);
+				if(gBoard.gameMap[i][j].getTileType()==1) {
+					g.drawImage(Assets.getInstance().getImage("TERRAIN_SAND"), tileLocation(gBoard.gameMap[i][j].getlocation().xIndex),
+							tileLocation(gBoard.gameMap[i][j].getlocation().yIndex), null);
 				}
-				if(gBoard.GameMap[i][j].getTileType()==2) {
-					g.drawImage(Assets.getInstance().getImage("TERRAIN_WATER"), tileLocation(gBoard.GameMap[i][j].getlocation().xIndex),
-							tileLocation(gBoard.GameMap[i][j].getlocation().yIndex), null);
+				if(gBoard.gameMap[i][j].getTileType()==2) {
+					g.drawImage(Assets.getInstance().getImage("TERRAIN_WATER"), tileLocation(gBoard.gameMap[i][j].getlocation().xIndex),
+							tileLocation(gBoard.gameMap[i][j].getlocation().yIndex), null);
 				}
 
 			}
