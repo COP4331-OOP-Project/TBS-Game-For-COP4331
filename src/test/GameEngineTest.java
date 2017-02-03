cbff4e7e5ff2e26e4b9c7b7d41ce3610bbab7102
R. Alex Clark <aclark2089@gmail.com>
2017-02-02 19:49:32 -0500

    Started testing directory

---
+++ src/test/GameEngineTest.java
@@ -0,0 +1,27 @@
+package test;
+
+import game.GameEngine;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Game Engine Testing
+ */
+class GameEngineTest {
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @AfterEach
+    void tearDown() {
+
+    }
+
+    @Test
+    void main() {
+
+    }
+
+}
