42d3ae5e4f2daf35a16fd7a9215c4b288c7987bf
Randy Brooks <randygeraldbrooks2@gmail.com>
2017-02-03 23:51:19 -0500

    Game_board

--- src/main/controls/ModeController.java
+++ src/main/controls/ModeController.java
@@ -1,8 +1,7 @@
 package controls;
 
 import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
+import org.apache.logging.log4j.Logger; 
 
 /**
  * Created by gavin on 2/1/17.
