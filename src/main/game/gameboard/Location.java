3c81dd6b792cdd0b6547eba6ce241b44af62c1fb
David Weng <dkweng@gmail.com>
2017-02-05 19:57:42 -0500

    Reverses made

--- src/main/game/gameboard/Location.java
+++ src/main/game/gameboard/Location.java
@@ -33,25 +33,25 @@ public class Location {
     public Location directionLocation(int direction){
         switch (direction){
             case 0:
-                return new Location(xIndex - 1 , yIndex);
+                return new Location(xIndex , yIndex - 1);
 
             case 45:
-                return new Location(xIndex - 1, yIndex + 1);
+                return new Location(xIndex + 1, yIndex - 1);
 
             case 90:
-                return new Location(xIndex, yIndex + 1);
+                return new Location(xIndex + 1, yIndex);
 
             case 135:
                 return new Location(xIndex + 1, yIndex + 1);
 
             case 180:
-                return new Location(xIndex + 1, yIndex);
+                return new Location(xIndex, yIndex + 1);
 
             case 225:
-                return new Location(xIndex + 1, yIndex - 1);
+                return new Location(xIndex - 1, yIndex + 1);
 
             case 270:
-                return new Location(xIndex, yIndex - 1);
+                return new Location(xIndex-1, yIndex);
 
             case 315:
                 return new Location(xIndex - 1, yIndex - 1);
