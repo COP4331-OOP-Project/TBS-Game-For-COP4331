ab063665a1baa20dc0b6ce4bd7a8f3e39e5694fb
Randy Brooks <randygeraldbrooks2@gmail.com>
2017-02-11 17:07:22 -0500

    Changed Camera Start Location

--- src/main/view/game/Camera.java
+++ src/main/view/game/Camera.java
@@ -3,17 +3,15 @@ package view.game;
 import game.Game;
 
 public class Camera {
-	private GamePanel gamePanel;
 	private PanelCenterer panelCenterer;
 	private static final int HEX_W = 100;
 	private static final int HEX_H = 86;
 	public Camera(GamePanel gamePanel, Game game) {
-		this.gamePanel = gamePanel;
 		this.panelCenterer = new PanelCenterer(gamePanel);
 	}
 	
-	private int offsetX = 0;
-	private int offsetY = 0;
+	private int offsetX = 180;
+	private int offsetY = -2340;
 	
 	protected void setX(int x) {
 		offsetX = x;
